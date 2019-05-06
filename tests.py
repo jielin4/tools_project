33f6b63860be255e220cbc6d47c842948df17e89
jielin4 <48304397+jielin4@users.noreply.github.com>
2019-05-06 15:41:41 -0400

    Update tests.py
    added description for the test file

--- tests.py
+++ tests.py
@@ -4,20 +4,20 @@ import unittest
 
 
 class Test(unittest.TestCase):
-
+#id
     def test_id(self):
         result = main.main_def()
         t_result = json.loads(s=result)
         ch = t_result["characterizations"]
         self.assertEqual(ch[3]["id"], 3)
-
+#singer
 
     def test_singer(self):
         result = main.main_def()
         t_result = json.loads(s=result)
         ch = t_result["characterizations"]
         self.assertEqual(ch[3]["artist"], "Jeff Beck")
-
+#name
     def test_name(self):
         result = main.main_def()
         t_result = json.loads(s=result)
